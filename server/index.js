f51e2ea741f480f93fce5d2abbdf73a51af361cf
TrinhVanHau <hauaclas@gmail.com>
2022-10-25 13:47:48 +0700

    test

---
+++ server/index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Signature = require('./models/Signature.js')
+const app = express();
+const url = 'mongodb://127.0.0.1:27017/signatures';
+const PORT = process.env.PORT || 3001;
+
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+//====MONGOOSE CONNECT===//
+mongoose.connect(url, function (err, db) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+    console.log('Connection established to', url);
+  }
+ });
+ //==========================//
+//====ROOT DIRECTORY===//
+  app.get('/', function(req, res) {
+    res.json('you did it');
+  });
+  //==========================//
+  //====GET ALL SIGNATURES===//
+  app.get('/api/signatures', function(req, res) {
+    Signature.find({}).then(eachOne => {
+      res.json(eachOne);
+      })
+    })
+  //==========================//
+  //====POST NEW SIGNATURE===//
+  app.post('/api/signatures', function(req, res) {
+    console.log(req.body);
+    Signature.create({
+    
+      message: req.body.message,
+    })
+  });
+  //==========================//
+  app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+  });
+
